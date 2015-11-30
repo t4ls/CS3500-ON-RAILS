240a759707f35a755252383ac1071dd17cf70785
Administrator <sysadmin@CS35F15-004.localdomain>
2015-11-30 23:38:52 +0000

    fixed runserver

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#Forwards port to vps and then starts rails
+#Forwards a port to vps and then starts rails
 #Works on any machine from anywhere for dev
-#http://t4ls.duckdns.org:3000/
-cd blog
-ssh -f -R 0.0.0.0:3000:localhost:3000 cclub@t4ls.duckdns.org -N 
-rails server -b 0.0.0.0 -p 3000 -u
+#Make sure to include port number
+#Example: ./runserver 3000
+echo http://t4ls.duckdns.org:$1
+ssh -f -R 0.0.0.0:$1:localhost:$1 cclub@t4ls.duckdns.org -N
+cd blog;sudo rails server -b 0.0.0.0 -p $1 -u
