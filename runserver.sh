0077e290d951aba5772fb21a7881e354b6ea9574
gigglesbw4 <blakewrege1@gmail.com>
2015-12-01 12:06:34 -0500

    fixed runserver script

--- runserver.sh
+++ runserver.sh
@@ -3,6 +3,12 @@
 #Works on any machine from anywhere for dev
 #Make sure to include port number
 #Example: ./runserver 3000
-echo http://t4ls.duckdns.org:$1
 ssh -f -R 0.0.0.0:$1:localhost:$1 cclub@t4ls.duckdns.org -N
+echo http://t4ls.duckdns.org:$1
+if [ "$(hostname)" == "CS35F15-004" ]; then
 cd blog;sudo rails server -b 0.0.0.0 -p $1 -u
+fi
+if [ "$(hostname)" != "CS35F15-004" ]; then
+cd blog;rails server -b 0.0.0.0 -p $1 -u
+fi
+
