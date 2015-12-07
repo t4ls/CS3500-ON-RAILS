501deadba567af3c90847cb1852a7e5f7bb22740
Administrator <sysadmin@CS35F15-004.localdomain>
2015-12-07 04:26:54 +0000

    stuff

---
+++ depot/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create(title: 'Test Product',
+  description: '<p> This is not a real product </p>',
+  image_url: 'test.jpg',
+  price: 19.99)
