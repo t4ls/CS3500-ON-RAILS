5bd3aae4bfc8b55f08712c3cf95a6ccf5056172a
Administrator <sysadmin@CS35F15-004.localdomain>
2015-12-16 04:36:16 +0000

    made basic user model

--- prey/db/schema.rb
+++ prey/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20121130000005) do
+ActiveRecord::Schema.define(version: 20151216042129) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at"
@@ -37,5 +37,15 @@ ActiveRecord::Schema.define(version: 20121130000005) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
