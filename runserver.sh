1482a6862bf0631bf3171f560f1e220cd5338744
gigglesbw4 <blakewrege1@gmail.com>
2015-12-15 21:05:29 -0500

    fixed runserver

--- runserver.sh
+++ runserver.sh
@@ -4,7 +4,7 @@
 #Make sure to include port number
 #Example: ./runserver 3000
 ssh -f -R 0.0.0.0:$2:localhost:$2 cclub@t4ls.duckdns.org -N
-echo URL FOR REVERSE TUNNEL: http://t4ls.duckdns.org:$1
+echo URL FOR REVERSE TUNNEL: http://t4ls.duckdns.org:$2
 if [ "$(hostname)" == "CS35F15-004" ]; then
 cd $1;sudo rails server -b 0.0.0.0 -p $2 -u
 fi
